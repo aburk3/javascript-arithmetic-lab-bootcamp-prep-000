4375accf443f7fd62b3edabe1766d036f4146624
aburk3 <aburk3@gmail.com>
2018-08-26 00:03:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,3 +17,49 @@ function divide(x, y) {
   return x / y
 }
 divide(5.0, 2.5)
+
+var number = 5
+
+number++ // 5... hmmm
+number // 6 -- the number was incremented after it was evaluated
+number-- // 6
+number // 5
+
+--number // 4 
+++number // 5
+
+number += 3 // 8 
+number -= 2 // 3 
+number *= 10 // 50 
+number /= 5 // 1 
+
+
+
+var number = 10 
+
+function add5() {
+  number += 5 
+}
+
+function divideBy3() {
+  number /= 3
+}
+
+divideBy3()
+
+console.log(number) // 3.333333335
+
+add5()
+
+console.log(number) // 8.333333335
+
+//reset number
+number = 10
+
+add5()
+
+console.log(number) // 15
+
+divideBy3()
+
+console.log(number) // 5 
